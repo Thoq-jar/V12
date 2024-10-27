568da7689a4af173c781695c5d699d7582d9eb03
Thoq-jar <tristanshaw000@gmail.com>
2024-10-27 17:26:23 -0400

    demo scripts

---
+++ examples/all.js
@@ -0,0 +1,20 @@
+// Loops
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let j = 2; j < 6; j++) {
+  console.log("Hey")
+}
+
+// IO
+console.log("Hello, V12!");
+console.log("Hello, World!");
+console.warn("Hello, warn!");
+console.error("Hello, error!");
+
+// Math [SOON]
+// const math = 1 + 1;
+// console.log(math);
+// eval(1+1);
+
